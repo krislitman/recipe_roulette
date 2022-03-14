88c398a4da6fbcb06a1b29ae431c8aee9309a156
Kris Litman <kris.d.litman@gmail.com>
2022-03-13 23:27:30 -0400

    Updated category spec with attributes

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,13 +3,20 @@ require "spec_helper.rb"
 RSpec.describe Category, type: :model do
     describe "A Category can be created" do
         before(:all) do
-            dataset = DB[:categories]
-            dataset.insert(name: "Breakfast")
-            @category = Category.where(name: "Breakfast")
+            @dataset = DB[:categories]
+            @dataset.insert(name: "Breakfast")
+            @category = Category.where(name: "Breakfast").first
+        end
+
+        after(:all) do
+            @dataset.where(name: "Breakfast").delete
         end
 
         it "Exists" do
             expect(@category).to be_an_instance_of(Category)
+            expect(@category.name).to eq("Breakfast")
+            expect(@category.category_id).to be_an(Integer)
+            expect(@category.recipe_id).to be_nil
         end
     end
 end
