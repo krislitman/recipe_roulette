fa710d6587b1846fac16b22d5b5fc7a7a54be2c1
Kris Litman <kris.d.litman@gmail.com>
2022-03-16 23:37:58 -0400

    Refactored controller to different singleton syntax

--- controllers/recipe_controller.rb
+++ controllers/recipe_controller.rb
@@ -1,7 +1,5 @@
 class RecipeController
-    class << self
-        def find_random_recipe(limit)
-            Recipe.get_random_recipe(limit)
-        end
+    def self.find_random_recipe(limit)
+        Recipe.get_random_recipe(limit)
     end
 end
