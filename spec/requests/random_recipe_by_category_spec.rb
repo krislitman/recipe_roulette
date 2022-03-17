f0a137e319e77473ec495e849321fc6076df68b6
Kris Litman <kris.d.litman@gmail.com>
2022-03-17 00:00:01 -0400

    Built out test to grab random recipe by category_id

--- spec/requests/random_recipe_by_category_spec.rb
+++ spec/requests/random_recipe_by_category_spec.rb
@@ -6,8 +6,66 @@ RSpec.describe App, roda: :app do
             @categories = DB[:categories]
             @categories.insert(name: "Breakfast")
             @breakfast = Category.where(name: "Breakfast").first
+            @categories.insert(name: "Lunch")
+            @lunch = Category.where(name: "Lunch").first
+            @categories.insert(name: "Dinner")
+            @dinner = Category.where(name: "Dinner").first
 
-            
+            @recipes = DB[:recipes]
+            10.times do
+                @recipes.insert(
+                    category_id: @breakfast.category_id,
+                    name: Faker::Food.dish,
+                    serves: Faker::Number.number(digits: 1),
+                    prep_time: "#{Faker::Number.number(digits: 1)} minutes",
+                    cooking_time: "#{Faker::Number.number(digits: 1)} minutes",
+                    ingredients: "#{Faker::Food.ingredient}|#{Faker::Food.ingredient}|#{Faker::Food.ingredient}|#{Faker::Food.ingredient}",
+                    instructions: "#{Faker::Food.measurement}|#{Faker::Food.measurement}|#{Faker::Food.measurement}",
+                    author: Faker::Hipster.word
+                )
+            end
+            10.times do
+                @recipes.insert(
+                    category_id: @lunch.category_id,
+                    name: Faker::Food.dish,
+                    serves: Faker::Number.number(digits: 1),
+                    prep_time: "#{Faker::Number.number(digits: 1)} minutes",
+                    cooking_time: "#{Faker::Number.number(digits: 1)} minutes",
+                    ingredients: "#{Faker::Food.ingredient}|#{Faker::Food.ingredient}|#{Faker::Food.ingredient}|#{Faker::Food.ingredient}",
+                    instructions: "#{Faker::Food.measurement}|#{Faker::Food.measurement}|#{Faker::Food.measurement}",
+                    author: Faker::Hipster.word
+                )
+            end
+            10.times do
+                @recipes.insert(
+                    category_id: @dinner.category_id,
+                    name: Faker::Food.dish,
+                    serves: Faker::Number.number(digits: 1),
+                    prep_time: "#{Faker::Number.number(digits: 1)} minutes",
+                    cooking_time: "#{Faker::Number.number(digits: 1)} minutes",
+                    ingredients: "#{Faker::Food.ingredient}|#{Faker::Food.ingredient}|#{Faker::Food.ingredient}|#{Faker::Food.ingredient}",
+                    instructions: "#{Faker::Food.measurement}|#{Faker::Food.measurement}|#{Faker::Food.measurement}",
+                    author: Faker::Hipster.word
+                )
+            end
+        end
+        after(:all) do
+            @recipes.where(category_id: @breakfast.category_id).delete
+            @recipes.where(category_id: @lunch.category_id).delete
+            @recipes.where(category_id: @dinner.category_id).delete
+
+            @categories.where(category_id: @breakfast.category_id).delete
+            @categories.where(category_id: @lunch.category_id).delete
+            @categories.where(category_id: @dinner.category_id).delete
+        end
+
+        it "Can find a random breakfast recipe given the breakfast category id" do
+            r = get "/api/v1/random/#{@breakfast.category_id}"
+            response = JSON.parse(r.body, symbolize_names: true)
+
+            expect(r.status).to eq(200)
+            expect(r.body).to be_a(String)
+            expect(response).to be_a(Hash)
         end
     end
 end
