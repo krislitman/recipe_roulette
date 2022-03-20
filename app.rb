9e6a207f04ad85d590309c9b58d54abe5b96d4e0
Kris Litman <kris.d.litman@gmail.com>
2022-03-20 02:19:50 -0400

    Refactored app.rb and moved controller logic to recipe_controller

--- app.rb
+++ app.rb
@@ -4,33 +4,11 @@ class App < Roda
     route do |r|
         r.on "api" do
             r.on "v1" do
-                r.on "index" do
-                    response = {
-                    message: "Welcome to Recipe Roulette!"
-                    }
-
-                    JSON.generate(response)
-                end
                 r.get "random" do
-                    limit = r.params[:limit] || 1
-                    recipe = RecipeController.find_random_recipe(limit, nil)
-
-                    if recipe
-                        JSON.generate(RecipeSerializer.new(recipe))
-                    else
-                        JSON.generate(ErrorMessage.missing(:recipe))
-                    end
+                    RecipeController.random(r.params[:limit], nil)
                 end
                 r.get "random", Integer do |category_id|
-                    limit = r.params[:limit] || 1
-                    category_id ||= nil
-                    recipe = RecipeController.find_random_recipe(limit, category_id)
-
-                    if recipe
-                        JSON.generate(RecipeSerializer.new(recipe))
-                    else
-                        JSON.generate(ErrorMessage.missing(:recipe))
-                    end
+                    RecipeController.random(r.params[:limit], category_id)
                 end
             end
         end
--- controllers/recipe_controller.rb
+++ controllers/recipe_controller.rb
@@ -1,5 +1,7 @@
 class RecipeController
-    def self.find_random_recipe(limit, category_id)
-        Recipe.get_random_recipe(limit, category_id)
+    def self.random(limit, category_id)
+        limit ||= 1
+        recipe = Recipe.get_random_recipe(limit, category_id) || ErrorMessage.missing(:recipe)
+        JSON.generate(RecipeSerializer.new(recipe))
     end
 end
